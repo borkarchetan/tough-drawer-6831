0267040b6016fa91504a3ff94f3d2e7ef2d41b96
Lenovo <battleshipcomputer@gmail.com>
2023-01-22 23:27:45 +0530

    TMS

--- TMS/src/com/tms/RUN/Main.java
+++ TMS/src/com/tms/RUN/Main.java
@@ -11,20 +11,21 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println();
-		System.out.println("*-*-*-*-*-*-*-*-*-*-*-*WELCOME TO RK TENDER MANAGEMENT STYSTEM*-*-*-*-*-*-*-*-*-*-*-*-*");
+		System.out.println(" ====================== WELCOME TO RK TENDER MANAGEMENT STYSTEM ====================== ");
 		System.out.println();
 		while (true) {
 
-			System.out.println("+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**+*+*");
+			System.out.println("==================================================================");
 			System.out.println("*                                                           +");
 
-			System.out.println("PRESS 1 FOR ADMINISTRATOR LOGIN");
+			System.out.println("ENTER 1 : ADMINISTRATOR LOGIN");
 
-			System.out.println("PRESS 2 FOR VENDOR LOGIN INTO APPLICATION");
+			System.out.println("ENTER 2 : VENDOR LOGIN INTO APPLICATION");
 
-			System.out.println("PRESS 3 TO CLOSE THE APPLICATION");
+			System.out.println("ENTER 3 : CLOSE THE APPLICATION");
 
-			System.out.println("+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**+*+*");
+			System.out.println();
+			System.out.println("==================================================================");
 			System.out.println();
 
 			int n = sc.nextInt();
